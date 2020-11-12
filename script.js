d021b16b2b8b001fffa3506da6c5a76a2f69279e
furnapso <luke.boland95@gmail.com>
2020-11-12 16:53:01 +1100

    Added grid creation function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function draw(gridsize) {
     // draw function
+
+    const grid = document.querySelector("#grid");
+    
+
+    for (let i = 0; i < parseInt(gridsize); i++) {
+        let div = document.createElement("div");
+        div.className = "cell";
+        grid.appendChild(div);
+    }
 }
 
 function clear() {
     // clear grid function
 }
+
+draw(16);
